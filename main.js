1ec84c9de71acf48abdc4df12ae1e3a5286a0b3e
Jen567892 <82474362+Jen567892@users.noreply.github.com>
2021-08-11 20:53:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,21 @@ function setup(){
 
     video = createCapture(VIDEO)
     video.size(400,400)
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPose);
 }
 
 function draw(){
+background("#A3EDFF");
+}
 
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+
+function gotPose(results){
+if(results.length > 0){
+    console.log(results);
+}
 }
